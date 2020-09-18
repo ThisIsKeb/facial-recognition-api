73cdc33eaa28c44eb649487bfb489555120ef3a3
ThisIsKeb <thisiskeb@gmail.com>
2020-09-17 20:26:01 -0400

    adding in back-end

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'ke3148vin',
+    port: '5433',
+    database : 'facial-recognition'
+  }
+});
+
+
+const app = express();
+
+//Middleware that parses the JSON body into JS that we can read
+app.use(express.json());
+//Using cors
+app.use(cors());
+
+// Sign-In also includes a more advanced function/call
+app.post('/signin', signin.handleSignIn(db, bcrypt))
+
+// Register
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+
+//Getting the users homepage
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+//Updating the image count
+app.put('/image', (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
+})
+
+
